321790337461ea76464ea527e94738a6d91bc6e3
Sujith Ranjitkumar <sujithranjitk@gmail.com>
2020-09-30 18:28:35 +1000

    Updates

---
+++ precache-manifest.5cb8a0c7f9ac17be252bc47cbf5a7b7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7be4d6b533a765d65cb847b6456a33",
+    "url": "/MBC-Employee-Directory/index.html"
+  },
+  {
+    "revision": "7d432c8afa0796acc482",
+    "url": "/MBC-Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9f72dfce9257b83484ca",
+    "url": "/MBC-Employee-Directory/static/css/main.256fb915.chunk.css"
+  },
+  {
+    "revision": "7d432c8afa0796acc482",
+    "url": "/MBC-Employee-Directory/static/js/2.47daf98b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MBC-Employee-Directory/static/js/2.47daf98b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f72dfce9257b83484ca",
+    "url": "/MBC-Employee-Directory/static/js/main.71315315.chunk.js"
+  },
+  {
+    "revision": "f5d6fd4ba56868858c0a",
+    "url": "/MBC-Employee-Directory/static/js/runtime-main.747f7307.js"
+  }
+]);
